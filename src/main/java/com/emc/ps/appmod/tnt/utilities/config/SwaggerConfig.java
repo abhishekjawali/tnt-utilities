562cd28cbe15e7adadc291628d8a2a0a5851a85c
abhishekjawali <abhishek.jawali@emc.com>
2016-10-03 14:11:08 +0530

    Update SwaggerConfig.java

--- src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
+++ src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("TNT Utilities Green").description("TNT Utilities Green").termsOfServiceUrl("")
+		return new ApiInfoBuilder().title("TNT Utilities").description("TNT Utilities").termsOfServiceUrl("")
 				.contact("").license("Apache License Version 2.0").licenseUrl("").version("1.0").build();
 	}
 }
