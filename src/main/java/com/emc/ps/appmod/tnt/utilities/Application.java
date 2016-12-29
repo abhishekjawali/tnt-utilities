6a494f97f99f37c497b75e463b7800372eb28a1e
Abhishek Jawali <abhishek.jawali@emc.com>
2016-12-29 09:18:39 +0530

    Changed to application.yml

--- src/main/java/com/emc/ps/appmod/tnt/utilities/Application.java
+++ src/main/java/com/emc/ps/appmod/tnt/utilities/Application.java
@@ -3,10 +3,12 @@ package com.emc.ps.appmod.tnt.utilities;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAutoConfiguration
 @RestController
