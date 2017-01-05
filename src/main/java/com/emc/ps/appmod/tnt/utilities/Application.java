565ef6a42ff61ef4f39cefb16024e688b738a9cd
Subramanyam <subramanyam.sattenapalli@emc.com>
2017-01-05 18:32:08 +0530

    Oauth related changes

--- src/main/java/com/emc/ps/appmod/tnt/utilities/Application.java
+++ src/main/java/com/emc/ps/appmod/tnt/utilities/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,17 @@ public class Application {
 	@RequestMapping(value = "/appInfo", method = RequestMethod.GET)
 	public String bookInfo() {
 		return "Utilities Microservice is running!";
+	}
+	
+	@Primary
+	@Bean
+	public RemoteTokenServices tokenService() {
+	    RemoteTokenServices tokenService = new RemoteTokenServices();
+	    tokenService.setCheckTokenEndpointUrl(
+	      "http://tnt-auth-server-oauth.cfapps.io/uaa/oauth/check_token");
+	    tokenService.setClientId("client");
+	    tokenService.setClientSecret("secret");
+	    return tokenService;
 	}
 
 }
