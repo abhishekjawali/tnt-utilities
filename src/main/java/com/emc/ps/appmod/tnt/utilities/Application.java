a907da22f05789ae717ed3846ec0d8b6ea70779d
Subramanyam <subramanyam.sattenapalli@emc.com>
2017-01-06 12:32:48 +0530

    Oauth changes

--- src/main/java/com/emc/ps/appmod/tnt/utilities/Application.java
+++ src/main/java/com/emc/ps/appmod/tnt/utilities/Application.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAutoConfiguration
 @RestController
